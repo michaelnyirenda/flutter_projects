4ec462e39b23a072e27de032d0a03d66dcf6fc49
duckielefleur <michael00nyirenda@gmail.com>
2022-04-12 00:20:56 +0200

    Revert "Push current android projects"
    This reverts commit 22db3bdd

--- lab1/app/src/welcome.kt
+++
@@ -1,3 +0,0 @@
-fun main() {
-    println("Welcome to Mobile Application Development (MAP711S)!")
-}
--- lab6/app/src/main/java/com/example/lab6/model/Affirmation.kt
+++
@@ -1,3 +0,0 @@
-package com.example.lab6.model
-
-data class Affirmation(val stringResourceId: Int)
